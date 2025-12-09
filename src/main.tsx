15c772a58ec95fca26b11b727eac654c94bf65de
celinedesnoe <cdesnoe@meilleursagents.com>
2025-12-09 17:30:25 +0100

    feat(router): add notfound and 403 pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import PaymentDetailsPage from "@/pages/PaymentDetails/PaymentDetailsPage";
 import PaymentsPage from "@/pages/Payments/PaymentsPage.tsx";
 
 import "./index.css";
+import ForbiddenPage from "./pages/Forbidden/ForbiddenPage";
+import NotFoundPage from "./pages/NotFound/NotFoundPage";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
@@ -14,6 +16,8 @@ createRoot(document.getElementById("root")!).render(
         <Route path="/" element={<Navigate to="/payments" replace />} />
         <Route path="/payments" element={<PaymentsPage />} />
         <Route path="/payments/:paymentId" element={<PaymentDetailsPage />} />
+        <Route path="/403" element={<ForbiddenPage />} />
+        <Route path="/*" element={<NotFoundPage />} />
       </Routes>
     </StrictMode>
   </BrowserRouter>
