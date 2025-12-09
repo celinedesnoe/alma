9893b8021d81e915d3082a5d9a0107b16a65d69d
celinedesnoe <cdesnoe@meilleursagents.com>
2025-12-09 17:23:34 +0100

    feat(Payments): add page

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+
 import PaymentDetailsPage from "@/pages/PaymentDetails/PaymentDetailsPage";
+import PaymentsPage from "@/pages/Payments/PaymentsPage.tsx";
+
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <StrictMode>
       <Routes>
         <Route path="/" element={<Navigate to="/payments" replace />} />
-        <Route path="/payments" element={<App />} />
+        <Route path="/payments" element={<PaymentsPage />} />
         <Route path="/payments/:paymentId" element={<PaymentDetailsPage />} />
       </Routes>
     </StrictMode>
