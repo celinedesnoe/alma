7a09939fff3596a67bcd12115ae7dcd16068d300
celinedesnoe <cdesnoe@meilleursagents.com>
2025-12-09 17:20:28 +0100

    feat(router): add routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import PaymentDetailsPage from "@/pages/PaymentDetails/PaymentDetailsPage";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <StrictMode>
+      <Routes>
+        <Route path="/" element={<Navigate to="/payments" replace />} />
+        <Route path="/payments" element={<App />} />
+        <Route path="/payments/:paymentId" element={<PaymentDetailsPage />} />
+      </Routes>
+    </StrictMode>
+  </BrowserRouter>
+);
